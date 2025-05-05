6953cc0b6a240ccc78b199124757f0e476be92b7
masayasusuzuki <suzukimasayasu.mailadress@gmail.com>
2025-05-05 21:54:25 +0900

    ver1.0.4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,7 +65,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-200 mt-12 pt-8 text-center">
-          <p className="opacity-60">
+          <p className="opacity-60 font-sans">
             &copy; {new Date().getFullYear()} Creative Portfolio. All rights reserved.
           </p>
         </div>
